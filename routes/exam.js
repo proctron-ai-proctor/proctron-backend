092f675295c913a4cce2fd4e09edd644ee403fb0
slayerdiangelo <slayerdiangelo@gmail.com>
2022-02-01 18:33:17 +0530

    RTCclass implementation

--- routes/exam.js
+++ routes/exam.js
@@ -1,30 +1,69 @@
-const express = require('express')
-const exam = express.Router()
 const wrtc = require('wrtc')
-const server = new wrtc.RTCPeerConnection()
 
+module.exports = class webRTCConnection {
+    constructor(socket, id){
+        const master = new wrtc.RTCPeerConnection()
+        var readyForIce = false
+        var remoteIceCandidates = []
 
-exam.post("/credentials/local-description", async (req, res) => {
-    try {
-        await server.setRemoteDescription(req.body)
-        const ans = await server.createAnswer()
-        await server.setLocalDescription(ans)
-        res.status(200).send(server.localDescription)
+        const drainRemoteIce = () => {
+            if(readyForIce) {
+                remoteIceCandidates.forEach (async(candidate) => {
+                    try {
+                        await master.addIceCandidate(candidate)
+                    }
+                    catch(error) {
+                        console.log(error)
+                    }
+                })
+                remoteIceCandidates = []
+            }
+        }
+        
+        master.onicecandidate = evt => {
+            if(evt.candidate)
+                socket.emit('credentials', {ice: evt.candidate})
+        }
+        master.onnegotiationneeded = async() => {
+            try {
+                const offer = await master.createOffer()
+                await master.setLocalDescription(offer)
+                socket.emit('credentials', {sdp: master.localDescription})
+            }
+            catch(error) {
+                console.log(error)
+            }
+        }
+        socket.on('exam/credentials', async({sdp, ice}) => {
+            try {
+                if(sdp) {
+                    await master.setRemoteDescription(sdp)
+                    if (sdp.type == 'offer') {
+                        const ans = await master.createAnswer()
+                        await master.setLocalDescription(ans)
+                        socket.emit('credentials', {sdp: master.localDescription})
+                        readyForIce = true
+                        drainRemoteIce()
+                    }
+                }
+                else if(ice)
+                {
+                    remoteIceCandidates.push(ice)
+                    drainRemoteIce()
+                }
+            }
+            catch(error) {
+                console.log(error)
+            }
+        })
+        master.ondatachannel = evt => {
+            const masterChannel = evt.channel
+            masterChannel.onmessage = evt => {
+                console.log(evt.data)
+            }
+            masterChannel.onopen = () => {
+                masterChannel.send('Hi')
+            } 
+        }
     }
-    catch(error){
-        console.log(error)
-    }
-})
-
-server.ondatachannel = evt => {
-    serverChannel = evt.channel
-    serverChannel.onmessage = evt => {
-        console.log(evt.data)
-    }
-    serverChannel.onopen = () => {
-        serverChannel.send('Hi')
-    }
-    
 }
-
-module.exports = exam;
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const auth = require('./routes/auth')
-const wrtc = require('wrtc')
+const master = require('./routes/exam')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const bodyParser = require('body-parser')
 
-const master = new wrtc.RTCPeerConnection()
-var readyForIce = false
-var remoteIceCandidates = []
 
 server.listen(3000)
 
@@ -17,64 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use("/users", auth)
 
 io.on('connection', socket => {
-    master.onicecandidate = evt => {
-        if(evt.candidate)
-            socket.emit('credentials', {ice: evt.candidate})
-    }
-    master.onnegotiationneeded = async() => {
-        try {
-            const offer = await master.createOffer()
-            await master.setLocalDescription(offer)
-            socket.emit('credentials', {sdp: master.localDescription})
-        }
-        catch(error) {
-            console.log(error)
-        }
-    }
-    drainRemoteIce = () => {
-        if(readyForIce) {
-            remoteIceCandidates.forEach (async(candidate) => {
-                try {
-                    await master.addIceCandidate(candidate)
-                }
-                catch(error) {
-                    console.log(error)
-                }
-            })
-            remoteIceCandidates = []
-        }
-    }
-    socket.on('exam/credentials', async({sdp, ice}) => {
-        try {
-            if(sdp) {
-                await master.setRemoteDescription(sdp)
-                if (sdp.type == 'offer') {
-                    const ans = await master.createAnswer()
-                    await master.setLocalDescription(ans)
-                    socket.emit('credentials', {sdp: master.localDescription})
-                    readyForIce = true
-                    drainRemoteIce()
-                }
-            }
-            else if(ice)
-            {
-                remoteIceCandidates.push(ice)
-                drainRemoteIce()
-            }
-        }
-        catch(error) {
-            console.log(error)
-        }
-    })
+    const id = socket.handshake.query.email
+    socket.join(id)
+    const newPeer = new master(socket, id)
 })
-
-master.ondatachannel = evt => {
-    masterChannel = evt.channel
-    masterChannel.onmessage = evt => {
-        console.log(evt.data)
-    }
-    masterChannel.onopen = () => {
-        masterChannel.send('Hi')
-    }
-    
-}
